6b164f1df9d97c6c3a3e35c4a8fcceb573a32441
Matias Villar <matiase.villar@gmail.com>
2023-04-20 12:19:38 -0600

    First & last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        
     }
 }
