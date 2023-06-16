f46ddf89b36d9f2b5b2eee87a341ff0c5d4ebf86
Matias Villar <matiase.villar@gmail.com>
2023-06-16 08:23:00 -0600

    deleted dev3

--- prove/Dev3/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Reference newReference = new Reference("John", 3, 16, 17);
-Console.WriteLine(newReference.StringifyRef());
-
-Scripture newScripture = new Scripture(newReference);
-newScripture.AddWordsToCreateScripture();
